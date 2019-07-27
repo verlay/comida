ba03071687059dd77cb9d17423215e882004dd63
verlay <stone_tlv_13@hotmail.com>
2019-07-27 14:09:52 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import UserNavigation from "./app/navigations/User";
+
+import firebaseConfig from "./app/utils/FireBase";
+import * as firebase from "firebase";
+firebase.initializeApp(firebaseConfig);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <UserNavigation />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
